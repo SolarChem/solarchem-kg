7e2fc7985112a92e111cec91678e34f9bf5747ac
Ana Iglesias <ana.iglesias.molina@gmail.com>
2024-12-16 17:54:28 +0100

    removing scripts

--- server-scripts/graph-update-curl.sh
+++
@@ -1,6 +0,0 @@
-curl -X 'POST' \
-  'https://solarchem.linkeddata.es/repositories/solarchem/rdf-graphs/service?default' \
-  -H 'accept: */*' \
-  -H 'Content-Type: text/plain' \
-  -H 'Authorization: GDB eyJ1c2VybmFtZSI6InNvbGFyY2hlbSIsImF1dGhlbnRpY2F0ZWRBdCI6MTcyNzg3MzIyMjM1MX0=.9FucE8drUU8SW2Aq0m4kbMDew61+4O3emKION6WbrPE=' \
-  --data-binary '@/home/solarchem/solarchem-kg/data/data.nt'
--- server-scripts/kg-construction.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source .venv/kgc/bin/activate
-python -m mapeathor -i ../mappings/outputs-solarchem-mapping.xlsx -l yarrrml -o ../mappings/outputs-solarchem-mapping
-python -m mapeathor -i ../mappings/solarchem-mapping.xlsx -l yarrrml -o ../mappings/solarchem-mapping
-python -m morph_kgc ../mappings/config.ini
-deactivate
